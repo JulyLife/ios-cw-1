bc194630597d1b600e40d27aa187288d5d2f6ef2
JulyLife-C60 <ghanima.sabti@gmail.com>
2020-09-09 20:28:14 +0300

    Done First HW & CW !

---
+++ CW-1-1.playground/Contents.swift
@@ -0,0 +1,16 @@
+let name = "Ghanima"
+var hobby1 = "Gaming"
+var hobby2 = "Drawing"
+var age = 15
+var height = 156
+print(" السلام عليكم، اسمي \(name) انا احب \(hobby1) و \(hobby2) عمري\(age) طولي\(height) ")
+
+let friendName = "Shaikha"
+var friendHobby1 = "Coding"
+var friendHobby2 = "Drawing"
+var friendAge = 25
+var friendHeight = 155
+var ageDifference =  friendAge - age
+
+var hobbyshare = (hobby1 == friendHobby1) || (hobby2 == friendHobby2) || (hobby1 == friendHobby2) || (hobby2 == friendHobby1)
+
